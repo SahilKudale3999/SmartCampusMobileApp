1767f66f85e106c4bbf6c95b2461c5a26cdeda66
SahilKudale3999 <sahilkudale2812@gmail.com>
2026-06-17 14:19:16 +0530

    Created packages

--- Backend/smart-campus/src/main/java/com/smartcampus/SmartCampusApplication.java
+++ Backend/smart-campus/src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartCampusApplication implements Runnable{
+public class SmartCampusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCampusApplication.class, args);
 	}
 
 
-	@Override
-	public void run() {
-		System.out.println("test");
-	}
+
 }
