b663b8be264f1d4e9a33f86e4c34ef0441892f8e
Prajwal Chitle <prajwalchitle38@gmail.com>
2026-08-06 15:33:50 +0530

    Add chatbot backend APIs

---
+++ Backend/smart-campus/src/main/java/com/smartcampus/dto/ChatResponse.java
@@ -0,0 +1,16 @@
+package com.smartcampus.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatResponse {
+
+	
+	private String response;
+}
